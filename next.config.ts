22808449abaed4b4ce1af4982c89943e45d538d7
Rajesh J Advani <rajeshjadvani@yahoo.com>
2025-08-16 15:24:49 +0530

    Added assetPrefix to current directory.

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,4 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -20,6 +20,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  assetPrefix: './',
 };
 
 export default nextConfig;
